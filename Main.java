46e795b3a015ee501ec6101f82f20f64d6423f3c
SANJAY S <113503336+Sanjay9384@users.noreply.github.com>
2025-05-29 21:19:13 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,52 @@
+package OOPS;
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		StudentManagementSystem SM = new StudentManagementSystem();
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("\n=== Menu ===");
+		    System.out.println("1. Add Student");
+		    System.out.println("2. Show All Students");
+		    System.out.println("3. Search Student by ID");
+		    System.out.println("4. Exit");
+		    System.out.print("Choose option: ");
+		    int choice = sc.nextInt();
+		    sc.nextLine();
+		    
+		    switch(choice) {
+		        
+		    case 1  :
+		    	SM.addStudent(sc);
+		    	break;
+		    case 2 :
+		    	SM.showStudent();
+		    	break;
+		    case 3 :
+		    	SM.searchStudent(sc);
+		    	break;
+		    case 4:
+		    	System.out.println("Are you sure want to exit(Yes/No) :");
+		    	String confirm = sc.nextLine();
+		    	
+		    	if(confirm.equals("Yes")) {
+		    		System.out.println("Exiting...");
+		    		return;
+		    	}
+		    	else {
+		    		System.out.println("Back to Menu");
+		    	}
+		    	break;
+		     default:
+		    	System.out.println("Invalid Option");
+		    	
+		    }
+		}
+		
+		
+		
+		
+	}
+
+}
---
+++ StudentManagementSystem.java
@@ -0,0 +1,79 @@
+package OOPS;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentManagementSystem {
+	private ArrayList<Student> students= new ArrayList<>();
+	
+	public void addStudent(Scanner sc) {
+		
+		System.out.println("How many students need to add : ");
+		int count = sc.nextInt();
+		
+		for(int i = 1; i <= count; i++) {
+			System.out.println("Enter the Id : ");
+			int id = sc.nextInt();
+			sc.nextLine();
+			
+			 System.out.print("Enter Student Name: ");
+		        String name = sc.nextLine();
+
+		        System.out.print("Enter Student Age: ");
+		        int age = sc.nextInt();
+		        sc.nextLine();
+
+		        System.out.print("Enter Student Department: ");
+		        String dept = sc.nextLine();
+		        
+		        Student student = new Student(id, name, age, dept);
+		        students.add(student);
+		        
+		        System.out.println("Student Added Successfully!");
+		}
+		
+		
+	}
+	
+	public void showStudent() {
+		
+		if(students.isEmpty()) {
+			System.out.println("No Student in the list");
+		}
+		else {
+			//To Show all Records
+			for(Student s : students) {
+				
+				System.out.println(s+"\n");
+		}
+		
+	}
+			
+	}
+	
+	public void searchStudent(Scanner sc) {
+		 System.out.print("\n Enter student ID to search: ");
+		    int searchId = sc.nextInt();
+		    sc.nextLine();
+		    
+		    boolean found = false;
+		    for(Student s : students) {
+		    	if(s.getId() == searchId) {
+		    		System.out.println("\n Student Found:");
+		            System.out.println("ID: " + s.getId());
+		            System.out.println("Name: " + s.getName());
+		            System.out.println("Age: " + s.getAge());
+		            System.out.println("Department: " + s.getDepartment());
+		            found = true;
+		            break;
+		    	}
+		    }
+		    if (!found) {
+		        System.out.println("❌ Student not found with ID: " + searchId);
+		    }
+		
+	}
+}
+	
+	
+
