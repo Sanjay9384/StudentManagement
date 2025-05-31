43cb39f01f7b59c62554f976bfd3d5e57c8028d0
SANJAY S <113503336+Sanjay9384@users.noreply.github.com>
2025-05-31 15:01:16 +0530

    Update StudentManagementSystem.java

--- StudentManagementSystem.java
+++ StudentManagementSystem.java
@@ -1,6 +1,6 @@
 
-
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class StudentManagementSystem {
@@ -69,8 +69,85 @@ public class StudentManagementSystem {
 		    	}
 		    }
 		    if (!found) {
-		        System.out.println("❌ Student not found with ID: " + searchId);
+		        System.out.println("X Student not found with ID: " + searchId);
 		    }
+		
+	}
+	
+	public void updateStudent(Scanner sc) {
+		System.out.println("Enter the Id of the student : ");
+		int updateId = sc.nextInt();
+		sc.nextLine();
+		
+		for(Student s: students) {
+			if(s.getId() == updateId) {
+				System.out.println("Which field needs to update \n1. Name\n2.Age\n3.Department");
+				int updateDetails = sc.nextInt();
+				String name = s.getName();
+				int age = s.getAge();
+				String dept =s.getDepartment();
+				sc.nextLine(); 
+				
+				switch(updateDetails) {
+				case 1:
+					System.out.print("Enter new Name: ");
+			         name = sc.nextLine();
+			        s.setName(name);
+			        System.out.println("Updated sucessfully!");
+			        break;
+			     
+				case 2:
+					 System.out.print("Enter Student Age: ");
+				       age = sc.nextInt();
+				      sc.nextLine();
+				      System.out.println("Updated sucessfully!");
+				      s.setAge(age);
+				      break;
+				case 3:
+					System.out.print("Enter Student Department: ");
+			         dept = sc.nextLine();
+			         System.out.println("Updated sucessfully!");
+			        s.setDepartment(dept);
+			        break;
+			    default:
+			    	System.out.println("Invalid Option to update!");
+			    	break;   
+				}
+				
+				
+				
+			}
+			else {
+				System.out.println("No Student not found");
+			}
+		}
+		
+		
+		
+	}
+	
+	public void deleteStudent(Scanner sc) {
+		System.out.println("Enter the Id of the student which you need to delete : ");
+		int id = sc.nextInt();
+		sc.nextLine();
+		
+		 boolean found = false;
+		    Iterator<Student> iterator = students.iterator();  // students is your ArrayList
+
+		while (iterator.hasNext()) {
+	        Student s = iterator.next();
+	        if (s.getId() == id) {
+	            iterator.remove();  // Safe removal inside loop
+	            found = true;
+	            System.out.println("✅ Student deleted successfully!");
+	            break;
+	        }
+	    }
+
+	    if (!found) {
+	        System.out.println("❌ Student with ID " + id + " not found.");
+	    }
+		
 		
 	}
 }
